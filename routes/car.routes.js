a8046b0e9e418eb5b840742256df4b78365e4768
N9ter <elkin.leva77@mail.ru>
2024-10-29 19:59:18 +0300

    debug

--- routes/car.routes.js
+++ routes/car.routes.js
@@ -1,9 +1,11 @@
-const { Router } = require("express");
-const router = new Router();
-const carController = require('.../controllers/car.controller.js');
-
-router.post('/api/car',carController.createCar);
-router.get('/api/car',carController.getCar);
-router.get('/api/car:id',carController.getCar);
-router.put('/api/car',carController.updateCar);
-router.get('/api/car:id',carController.deleteCar);
+const { Router } = require("express"); 
+const router = new Router(); 
+const carController = require('../controllers/car.controller.js'); 
+ 
+router.post('/api/car',carController.createCar); 
+router.get('/api/car',carController.getCars); 
+router.get('/api/car:id',carController.getCar); 
+router.put('/api/car',carController.updateCar); 
+router.delete('/api/car:id',carController.deleteCar); 
+ 
+module.exports = Router;
